22d3f498978b8b4d297d5b2921dd7ec50321bd47
maslri <ma.slri2128@gmail.com>
2024-03-02 23:19:18 +0330

    fill all models

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 
 # Create your models here.
 class Category(models.Model):
-    
+    parent = models.ForeignKey('self', verbose_name=_('parent'), null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), upload_to='categories/', blank=True)
@@ -17,7 +17,28 @@ class Category(models.Model):
         verbose_name_plural = _('categories')
 
 class Product(models.Model):
-    pass
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), upload_to='products/', blank=True)
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField(_('Category'), verbose_name=_('categories'), blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/', blank=True)
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
