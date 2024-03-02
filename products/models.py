141d6f758bfcbee79173dbe73e455978c8c49748
maslri <ma.slri2128@gmail.com>
2024-03-02 23:10:07 +0330

    first committed

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 # Create your models here.
+class Category(models.Model):
+    
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), upload_to='categories/', blank=True)
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+class Product(models.Model):
+    pass
+
+class File(models.Model):
+    pass
